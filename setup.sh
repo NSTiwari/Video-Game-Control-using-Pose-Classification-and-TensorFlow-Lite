4f1bac39b26693f38b147be1a4120add0384b910
Nitin Tiwari <37960279+NSTiwari@users.noreply.github.com>
2022-01-13 00:42:10 +0530

    Setup.sh file to download required libraries and dependencies.

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  DATA_DIR="./"
+else
+  DATA_DIR="$1"
+fi
+
+# Install Python dependencies
+python3 -m pip install pip --upgrade
+python3 -m pip install -r requirements.txt
